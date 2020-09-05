69e2c12df944883e6a8a6f0d0f40a652c22116f0
NuhMohammed <Mohammednuuh@gmail.com>
2020-09-05 13:47:04 +0300

    solved error raised in Procfile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"moalinmoalin2@gmail.com"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "\[general]\n\ email = \"moalinmoalin2@gmail.com"\n\"
+ > ~/.streamlit/credentials.toml
+echo "\[server]\n\ headless = true\n\ enableCORS=false\n\ port = $PORT\n\"
+
+> ~/.streamlit/config.toml
