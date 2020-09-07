0f6b4b507357e12de2a5a2dc645f5a4fbc80afd6
NuhMohammed <Mohammednuuh@gmail.com>
2020-09-07 13:07:31 +0300

    Correct Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:sh setup.sh && streamlit run app.py apps
+web: sh setup.sh && streamlit run app.py
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "\[general]\n\ email = \"moalinmoalin2@gmail.com"\n\"
- > ~/.streamlit/credentials.toml
-echo "\[server]\n\ headless = true\n\ enableCORS=false\n\ port = $PORT\n\"
 
-> ~/.streamlit/config.toml
+echo "\
+[general]\n\
+email = \"moalinmoalin2@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
