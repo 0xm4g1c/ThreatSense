78900b4ad228eb82d94ec803dda170cd5898c742
NuhMohammed <Mohammednuuh@gmail.com>
2020-09-07 16:13:07 +0300

    configured environments

---
+++ env_credentials.py
@@ -0,0 +1,13 @@
+from os import environ
+
+FILE_KEY = environ['FILE_KEY']
+FILE_SECRET = environ['FILE_SECRET']
+
+IP_KEY = environ['IP_KEY']
+IP_SECRET = environ['IP_SECRET']
+
+URL_KEY = environ['URL_KEY']
+URL_SECRET = environ['URL_SECRET']
+
+print (FILE_SECRET)
+
